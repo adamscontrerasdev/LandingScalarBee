a816d03e574c52ed4f0d8af0bd5adb5650f8bb44
AdamsNxt <86929251+adamscontrerasdev@users.noreply.github.com>
2025-04-04 15:42:07 -0300

    fix EsLint

--- src/app/Screens/InicioScreen.tsx
+++ src/app/Screens/InicioScreen.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Content } from "../components";
 import { useTheme } from "../Hooks/themeContext";
 import Image from "next/image";
