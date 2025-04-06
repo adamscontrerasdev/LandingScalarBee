ac5abc2a562e6658b8bcb9f28e23d7329d8a7cbe
Reinapepeada <103573136+Reinapepeada@users.noreply.github.com>
2025-04-06 19:20:36 -0300

    hot fix metallic print

--- src/app/components/InicioComponents/Content.tsx
+++ src/app/components/InicioComponents/Content.tsx
@@ -3,45 +3,49 @@ import { ButtonPrimary, FadeContent, Title, SubTitle } from "@/app/Elements";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTheme } from "@/app/Hooks/themeContext";
-import logo from "./react-bits-solid-black-CFfyrfKs.svg";
 import MetallicPaint, {
-  parseLogoImage,
+    parseLogoImage,
 } from "./../../../../Elements/MetallicPaint/MetallicPaint";
 
 export const Content = () => {
-  const { isDarkMode } = useTheme();
-  const [imageData, setImageData] = useState<ImageData | null>(null);
+    const { isDarkMode } = useTheme();
+    const [imageData, setImageData] = useState<ImageData | null>(null);
 
-  useEffect(() => {
-    async function loadDefaultImage() {
-      try {
-        const response = await fetch(logo);
-        const blob = await response.blob();
-        const file = new File([blob], "react-bits-solid-black-CFfyrfKs.svg", {
-          type: blob.type,
-        });
+    useEffect(() => {
+        async function loadDefaultImage() {
+            try {
+                // Crear un archivo directamente desde la URL importada
+                // console.log(logo)
+                const response = await fetch(
+                    "/Img/logo scalarbee/Abeja.svg"
+                );
+                console.log(response);
+                const blob = await response.blob();
+                const file = new File([blob], "default.png", {
+                    type: blob.type,
+                });
 
-        const parsedData = await parseLogoImage(file);
-        setImageData(parsedData?.imageData ?? null);
-      } catch (err) {
-        console.error("Error loading default image:", err);
-      }
-    }
+                const parsedData = await parseLogoImage(file);
+                console.log(parsedData);
+                setImageData(parsedData?.imageData ?? null);
+            } catch (err) {
+                console.error("Error loading default image:", err);
+            }
+        }
 
-    loadDefaultImage();
-  }, []);
+        loadDefaultImage();
+    }, []);
 
-  return (
-    <FadeContent
-      blur={true}
-      duration={1000}
-      easing="ease-out"
-      initialOpacity={0}
-      className=" h-full flex flex-col lg:flex-row-reverse justify-center items-center  rounded-2xl  z-10"
-    >
-      {/* Imagen Arriba en Mobile, Derecha en Desktop */}
-      <div className="w-full h-1/2 lg:h-auto flex justify-center lg:justify-start items-center ">
-        {/* <Image
+    return (
+        <FadeContent
+            blur={true}
+            duration={1000}
+            easing="ease-out"
+            initialOpacity={0}
+            className=" h-full flex flex-col lg:flex-row-reverse justify-center items-center  rounded-2xl  z-10">
+            {/* Imagen Arriba en Mobile, Derecha en Desktop */}
+            <div className="w-full h-1/2 lg:h-auto flex justify-center lg:justify-start items-center ">
+                {/* <Image
           src={
             isDarkMode
               ? "/Img/Inicio/ResponsiveDark.png"
@@ -52,47 +56,44 @@ export const Content = () => {
           alt="QLQ"
           className="w-full object-contain max-w-2xl "
         /> */}
-        <div style={{ width: "100%", height: "100vh", zIndex: 9999999 }}>
-          <MetallicPaint
-            imageData={imageData ?? new ImageData(1, 1)}
-            params={{
-              edge: 2,
-              patternBlur: 0.005,
-              patternScale: 2,
-              refraction: 0.015,
-              speed: 0.3,
-              liquid: 0.07,
-            }}
-          />
-        </div>
-      </div>
+                    <MetallicPaint
+                      imageData={imageData ?? new ImageData(1,1)}
+                      params={{
+                        patternScale: 2,
+                        refraction: 0,
+                        edge: 0,
+                        patternBlur: 0,
+                        liquid: 0,
+                        speed: 0.15,
+                      }}
+                    />
+            </div>
 
-      {/* Contenido de Texto */}
-      <div className="w-full h-1/2 md:h-full flex justify-center items-start md:items-center  md:p-5 ">
-        <div
-          className="flex flex-col items-start justify-center gap-3  md:p-5"
-          style={{
-            background:
-              "linear-gradient(150deg, var(--background) 30%, transparent 40%)",
-            borderRadius: "20px 0 0 0",
-          }}
-        >
-          <Title
-            value="Tu laboratorio de reparación al siguiente nivel."
-            left
-          />
-          <SubTitle
-            value="Gestiona tu negocio de móviles como lo hacen los mejores:
+            {/* Contenido de Texto */}
+            <div className="w-full h-1/2 md:h-full flex justify-center items-start md:items-center  md:p-5 ">
+                <div
+                    className="flex flex-col items-start justify-center gap-3  md:p-5"
+                    style={{
+                        background:
+                            "linear-gradient(150deg, var(--background) 30%, transparent 40%)",
+                        borderRadius: "20px 0 0 0",
+                    }}>
+                    <Title
+                        value="Tu laboratorio de reparación al siguiente nivel."
+                        left
+                    />
+                    <SubTitle
+                        value="Gestiona tu negocio de móviles como lo hacen los mejores:
 organiza, controla y crece sin perder el control."
-          />
-          <SubTitle
-            value="Hecho para técnicos que quieren más que sobrevivir:
+                    />
+                    <SubTitle
+                        value="Hecho para técnicos que quieren más que sobrevivir:
 quieren dominar."
-          />
+                    />
 
-          <ButtonPrimary text="Demo Exlusiva" />
-        </div>
-      </div>
-    </FadeContent>
-  );
+                    <ButtonPrimary text="Demo Exlusiva" />
+                </div>
+            </div>
+        </FadeContent>
+    );
 };
