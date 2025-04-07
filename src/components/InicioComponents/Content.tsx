38042d894c3e6609bbe7e1104cc466fb61d95207
Reinapepeada <103573136+Reinapepeada@users.noreply.github.com>
2025-04-07 13:12:24 -0300

    update

--- src/components/InicioComponents/Content.tsx
+++ src/components/InicioComponents/Content.tsx
@@ -10,6 +10,7 @@ import MetallicPaint, {
     parseLogoImage,
 } from "@/components/Elements/MetallicPaint";
 import RotatingText from "@/components/Elements/RotatingText";
+import { MdPhonelinkSetup } from "react-icons/md";
 export const Content = () => {
     const [imageData, setImageData] = useState<ImageData | null>(null);
 
@@ -35,77 +36,90 @@ export const Content = () => {
     }, []);
 
     return (
-      <>
-        <FadeContent
-            blur={true}
-            duration={1000}
-            easing="ease-out"
-            initialOpacity={0}
-            className=" h-full flex flex-col lg:flex-row-reverse justify-center items-center  rounded-2xl  z-10 ">
-            {/* Imagen Arriba en Mobile, Derecha en Desktop */}
-            <div className=" flex justify-center items-center w-full h-1/2 lg:h-auto lg:p-32 max-w-3xl">
-                {imageData && (
-                    <FadeContent
-                        blur={true}
-                        duration={1000}
-                        easing="ease-out"
-                        initialOpacity={0}>
-                        <MetallicPaint
-                            imageData={imageData}
-                            params={{
-                                patternScale: 2,
-                                refraction: 0,
-                                edge: 0,
-                                patternBlur: 0,
-                                liquid: 0,
-                                speed: 0.15,
-                            }}
-                        />
-                    </FadeContent>
-                )}
-            </div>
+        <>
+            <FadeContent
+                blur={true}
+                duration={1500}
+                easing="ease-out"
+                initialOpacity={0}
+                className=" h-full flex flex-col lg:flex-row-reverse justify-center items-center  rounded-2xl  z-10 ">
+                {/* Imagen Arriba en Mobile, Derecha en Desktop */}
+                <div className=" flex justify-center items-center w-full h-1/2 lg:h-auto lg:p-32 max-w-3xl">
+                    {imageData && (
+                        <FadeContent
+                            blur={true}
+                            duration={1500}
+                            easing="ease-out"
+                            initialOpacity={0}>
+                            <MetallicPaint
+                                imageData={imageData}
+                                params={{
+                                    patternScale: 2,
+                                    refraction: 0,
+                                    edge: 0,
+                                    patternBlur: 0,
+                                    liquid: 0,
+                                    speed: 0.15,
+                                }}
+                            />
+                        </FadeContent>
+                    )}
+                </div>
 
-            {/* Contenido de Texto */}
-            <div className="w-full h-1/2 md:h-full flex justify-center items-start lg:items-center  lg:p-5  ">
-                <div
-                    className="flex flex-col items-start justify-center gap-5 md:p-5"
-                    style={{
-                        background:
-                            "linear-gradient(150deg, var(--background) 30%, transparent 15%)",
-                        borderRadius: "30px 0 0 0",
-                    }}>
-                    <h1
-                        className={`font-bold text-[var(--texts)] 
-                          text-3xl md:text-3xl xl:text-5xl 2xl:text-6xl
+                {/* Contenido de Texto */}
+                <div className="w-full h-1/2 md:h-full flex justify-center items-start lg:items-center  lg:p-5  ">
+                    <div
+                        className="flex flex-col items-start justify-center gap-5 md:p-5"
+                        style={{
+                            background:
+                                "linear-gradient(150deg, var(--background) 30%, transparent 15%)",
+                            borderRadius: "30px 0 0 0",
+                        }}>
+                        <div className="flex items-center gap-3">
+                            <h1
+                                className={`font-bold text-[var(--primary)]
+                            text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl
+                            text-left  `}>
+                                Beefix
+                            </h1>
+                            <MdPhonelinkSetup className=" text-[var(--texts)] text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl" />
+                            <RotatingText
+                                texts={[
+                                    "Controla",
+                                    "Administra",
+                                    "Ordena",
+                                    "Gestiona",
+                                ]}
+                                mainClassName="text-xl xl:text-3xl p-4 sm:px-2 font-bold md:px-3 bg-[var(--primary)] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                                staggerFrom={"last"}
+                                initial={{ y: "100%" }}
+                                animate={{ y: 0 }}
+                                exit={{ y: "-120%" }}
+                                staggerDuration={0.025}
+                                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                                transition={{
+                                    type: "spring",
+                                    damping: 30,
+                                    stiffness: 400,
+                                }}
+                                rotationInterval={2500}
+                            />
+                        </div>
+                        <h2
+                            className={`text-[var(--texts)] 
+                          text-3xl md:text-3xl xl:text-4xl 2xl:text-4xl
                           text-left  `}>
-                        Donde la reparación se convierte en negocio.
-                    </h1>
-                    <RotatingText
-                        texts={["Controla", "Administra", "Ordena", "Gestiona"]}
-                        mainClassName="text-xl xl:text-3xl p-4 sm:px-2 font-bold md:px-3 bg-[var(--primary-dark)] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-                        staggerFrom={"last"}
-                        initial={{ y: "100%" }}
-                        animate={{ y: 0 }}
-                        exit={{ y: "-120%" }}
-                        staggerDuration={0.025}
-                        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                        transition={{
-                            type: "spring",
-                            damping: 30,
-                            stiffness: 400,
-                        }}
-                        rotationInterval={2500}
-                    />
-                    {/* <SubTitle
+                            Gestiona tu servicio técnico como lo hacen los profesionales.
+                        </h2>
+                        {/* <SubTitle
             value="Hecho para técnicos que quieren más que sobrevivir:
 quieren dominar."
           /> */}
 
-                    {/* <ButtonPrimary text="Demo Exlusiva" /> */}
+                        {/* <ButtonPrimary text="Demo Exlusiva" /> */}
+                    </div>
                 </div>
-            </div>
-        </FadeContent>
-            
-      </>
+            </FadeContent>
+        </>
     );
 };
--- src/components/NavbarComponent/Navbar.tsx
+++ src/components/NavbarComponent/Navbar.tsx
@@ -15,7 +15,18 @@ export const Navbar = () => {
     return (
         <div className="w-screen h-20 flex justify-between items-center  fixed top-0 z-[99999999] left-0 p-3 md:p-0 md:px-5 xl:px-10 backdrop-blur-xs ">
             <div className="h-1/2 w-auto  flex gap-2 justify-center items-center">
-                      {/* <Image
+            <Image
+                src={
+                    isDarkMode
+                        ? "/Img/logo scalarbee/bee-logo-white.png"
+                        : "/Img/logo scalarbee/bee-logo-black.png"
+                }
+                alt="Logo"
+                width={1272}
+                height={750}
+                className="w-auto h-full"
+            />
+                      <Image
                 src={
                   isDarkMode
                     ? "/Img/logo scalarbee/bee-text-white.png"
@@ -25,19 +36,8 @@ export const Navbar = () => {
                 width={1272}
                 height={750}
                 className="w-auto h-full hidden md:block"
-              /> */}
-                <Image
-                    src={
-                        isDarkMode
-                            ? "/Img/logo scalarbee/bee-logo-white.png"
-                            : "/Img/logo scalarbee/bee-logo-black.png"
-                    }
-                    alt="Logo"
-                    width={1272}
-                    height={750}
-                    className="w-auto h-full"
-                />
-                <p className="text-3xl">{`BeeFix`}</p>
+              />
+                {/* <p className="text-3xl">{`BeeFix`}</p> */}
             </div>
             <div className="h-1/2 min-w-20">
                 {/* {roots.map((root) => (
